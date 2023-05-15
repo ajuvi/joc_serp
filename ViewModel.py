285818876f2e96b374af6fea1e74dc162d5b3e88
ajuvi <102821845+ajuvi@users.noreply.github.com>
2023-05-15 06:30:01 +0200

    artur

--- ViewModel.py
+++ ViewModel.py
@@ -33,12 +33,6 @@ class ViewModel:
         self._loadTile('cua',"./imatges/cua.png")
         self._loadTile('poma',"./imatges/poma.png")
 
-        #crear la serp
-        self.serp=None
-        
-        #crear la poma
-        self.poma=None
-
     def _loadTile(self,key,url):
         print(url)
         img = pygame.image.load(url) 
