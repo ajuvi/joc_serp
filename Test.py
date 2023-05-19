300f9e1e73f7b900b069d71ce76e7c183bdc76f4
ajuvi <102821845+ajuvi@users.noreply.github.com>
2023-05-19 21:05:59 +0200

    artur

--- Test.py
+++ Test.py
@@ -1,13 +1,19 @@
 from models.Poma import Poma
 from models.Serp import Serp
+from ViewModel import ViewModel
+
+#obtenir paràmetres del joc
+vm = ViewModel();
+tile_width = vm.tile_width;
+tile_height = vm.tile_height;
+imatge_poma = vm.getTile('poma');
+imatge_serp = vm.getTile('serp');
+direccio_serp=[1,0]
 
 #crear un objecte de poma
-poma = Poma(1,1,10,10,None)
-#mostrar dades de la poma
-print("x_poma: ",poma.x)
-print("y_poma: ",poma.y)
+poma = Poma(1,1,tile_width,tile_height,imatge_poma)
+print("x_poma: ",poma.x,"y_poma: ",poma.y)
 
-#posicionar la poma en la posició (9,9)
-poma.moure(9,9)
-print("x_poma: ",poma.x)
-print("y_poma: ",poma.y)
+#crear un objecte de serp
+serp = Serp(10,10,tile_width,tile_height,direccio_serp, imatge_serp)
+print("x_serp: ",serp.x,"y_serp: ",serp.y)
