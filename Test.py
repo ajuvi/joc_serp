9a8700e1ff4147b1b9b879f9a948a289554c5fd5
ajuvi <102821845+ajuvi@users.noreply.github.com>
2023-05-19 20:42:07 +0200

    artur

--- Test.py
+++ Test.py
@@ -1,5 +1,13 @@
 from models.Poma import Poma
-from ViewModel import ViewModel
+from models.Serp import Serp
 
-poma = Poma(1,1,10,10)
-print(poma)
+#crear un objecte de poma
+poma = Poma(1,1,10,10,None)
+#mostrar dades de la poma
+print("x_poma: ",poma.x)
+print("y_poma: ",poma.y)
+
+#posicionar la poma en la posició (9,9)
+poma.moure(9,9)
+print("x_poma: ",poma.x)
+print("y_poma: ",poma.y)
